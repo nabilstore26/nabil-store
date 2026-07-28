b3210f327f0ea86ea647bd607d38f8ae1c36f34a
nabilstore26 <nabil2025m55@gmail.com>
2026-07-28 22:57:59 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// كود إعدادات Firebase الخاص بمشروع Nabil-Biznes
+const firebaseConfig = {
+  apiKey: "AIzaSyDo-3mcY6X602sQDTCev-OZG2MK7eWLYoE",
+  authDomain: "nabil-biznes.firebaseapp.com",
+  projectId: "nabil-biznes",
+  storageBucket: "nabil-biznes.firebasestorage.app",
+  messagingSenderId: "389434271966",
+  appId: "1:389434271966:web:910c029ed8ecf06e4c3a4",
+  measurementId: "G-KZPD4JRQ5G"
+};
+
+// تهيئة تطبيق Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+
+// تصدير العناصر لاستخدامها في باقي ملفات المشروع
+export { app, analytics };
+export default app;
 
